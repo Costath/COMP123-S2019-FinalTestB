7969651b304f48fc4ba72dc2a2d6698351736ad7
Thales Costa <thalescosta@outlook.com>
2019-08-12 13:06:47 -0400

    Added Character Classes and files containing Character names

--- COMP123-S2019-FinalTestB/Objects/Character.cs
+++ COMP123-S2019-FinalTestB/Objects/Character.cs
@@ -14,7 +14,7 @@ using System.Threading.Tasks;
 namespace COMP123_S2019_FinalTestB.Objects
 
 {
-    class Character
+    public class Character
     {
         // Character Abilitits
         public string Strength { get; set; }
@@ -29,7 +29,7 @@ namespace COMP123_S2019_FinalTestB.Objects
         public int HitPoints { get; set; }
 
         // Character Class
-        public string CharacterClass { get; set; }
+        public CharacterClass Class { get; set; }
         public int Level { get; set; }
 
         // Equipment
